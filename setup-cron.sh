d5cc5386e3e33bf5b36f901d6d904fa534a14852
ReeseHatfield <reesehatfield02@gmail.com>
2026-03-04 18:27:11 -0500

    cron job

---
+++ setup-cron.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DUCK="$(pwd)/duck.sh"
+
+
+if [ ! -f "$DUCK" ]; then
+
+    echo "could not find duck.sh in cwd"
+    exit 1
+fi
+
+
+(crontab -l 2>/dev/null; echo "*/5 * * * * $DUCK >/dev/null 2>&1") | crontab -
+
+echo "Installed new cron job"
+echo "Will update duckdns ip every 5 minutes"
