9a1d7ac23e6a2dcaf488cc617c10009e276c2126
ReeseHatfield <reesehatfield02@gmail.com>
2026-03-06 11:10:49 -0500

    printable messages

--- hellm-client/client.py
+++ hellm-client/client.py
@@ -2,26 +2,45 @@
 
 import shutil
 
-width, height = shutil.get_terminal_size()
+
+
+from enum import Enum
+
+class Justify(Enum):
+    LEFT = 1
+    RIGHT = 2
+
+def print_message(text: str, align: Justify):
+    
+    
+    width, _ = shutil.get_terminal_size()
+
+    formatted_str = ""
+    counter = 0
+    cur_row = ""
+        
+    for c in text:
+        
+        cur_row += c
+        counter += 1
+        
+        if counter > width / 2:
+            counter = 0
+            
+            if align == Justify.LEFT:
+                cur_row = cur_row.rjust(width, ' ')
+            elif align == Justify.RIGHT:
+                cur_row = cur_row.ljust(width, ' ')
+                
+                
+            cur_row += '\n'        
+            
+            formatted_str += cur_row
+            cur_row = ""
+            
+            
+    print(formatted_str)
+
 
 lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
-
-formatted_str = ""
-counter = 0
-cur_row = ""
-for c in lorem:
-    
-    cur_row += c
-    counter += 1
-    
-    if counter > width / 2:
-        counter = 0
-        cur_row = cur_row.ljust(width, ' ')
-        cur_row += '\n'        
-        
-        formatted_str += cur_row
-        cur_row = ""
-        
-
-print(formatted_str)
+print_message(lorem, align=Justify.LEFT)
